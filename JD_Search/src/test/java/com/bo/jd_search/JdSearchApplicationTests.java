79d0624d87eefb4ad1c4a637593ff5191d0bc194
Bo_Vane <884851491@qq.com>
2023-11-21 15:00:33 +0800

    Initial commit

---
+++ JD_Search/src/test/java/com/bo/jd_search/JdSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bo.jd_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
